491baec6df0bd307d3f452d396bb482b6c1682dd
unknown <https://github.com/bhuwanchaudhary715/Java-with-Dsa.git>
2024-09-24 05:09:49 +0530

    Fetching Image

---
+++ src/main/java/com/pwskills/ecom_project/service/ProductService.java
@@ -0,0 +1,42 @@
+package com.pwskills.ecom_project.service;
+
+import com.pwskills.ecom_project.model.Product;
+import com.pwskills.ecom_project.repository.ProductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepo repo;
+
+
+    public List<Product> getAllProduct() {
+        return repo.findAll();
+
+    }
+
+    public Product getProductById(int id) {
+        return repo.findById(id).orElse( null);
+    }
+
+
+    public Product addProduct(Product product, MultipartFile imageFile) throws IOException {
+        product.setImageName(imageFile.getOriginalFilename());
+        product.setImageType(imageFile.getContentType());
+        product.setImageDate(imageFile.getBytes());
+        return repo.save(product);
+    }
+
+
+
+}
